8174598494fa31d5926a768e7c18752103505643
pkda1lu <antohaburda2006@gmail.com>
2026-04-24 16:28:01 +0300

    cdn

--- server/routes/channels.js
+++ server/routes/channels.js
@@ -27,7 +27,7 @@ router.post('/', auth, checkPermission(Permissions.MANAGE_CHANNELS, 'body.server
     });
     const io = req.app.get('io');
     if (io) {
-      const updatedServer = await Server.findById(serverId).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges');
+      const updatedServer = await Server.findById(serverId).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges activity');
       io.to(`server-${serverId}`).emit('server-updated', updatedServer);
     }
     res.status(201).json(channel);
@@ -77,7 +77,7 @@ router.put('/:id', auth, async (req, res, next) => {
 
     const io = req.app.get('io');
     if (io) {
-      const updatedServer = await Server.findById(channel.server).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges');
+      const updatedServer = await Server.findById(channel.server).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges activity');
       io.to(`server-${channel.server}`).emit('server-updated', updatedServer);
     }
     res.json(channel);
@@ -112,7 +112,7 @@ router.delete('/:id', auth, async (req, res, next) => {
     }
     const io = req.app.get('io');
     if (io) {
-      const updatedServer = await Server.findById(serverId).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges');
+      const updatedServer = await Server.findById(serverId).populate('owner', 'username avatar badges').populate('channels').populate('members.user', 'username avatar status badges activity');
       io.to(`server-${serverId}`).emit('server-updated', updatedServer);
     }
     res.json({ message: 'Channel deleted' });
